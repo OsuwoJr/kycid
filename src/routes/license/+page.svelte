bf46e43b7a7246313e56d02fc81c99bede648974
OsuwoJr <johnfredosuwo@gmail.com>
2024-08-21 16:14:57 +0300

    Update the license page

--- src/routes/license/+page.svelte
+++ src/routes/license/+page.svelte
@@ -1,1 +1,33 @@
-license 
+<div class=" text-white p-8">
+    <div class="max-w-4xl mx-auto  p-6 rounded-lg shadow-md">
+      <h1 class="text-2xl font-bold mb-4 ">MIT License</h1>
+      <p class="mb-4 ">
+        Copyright (c) <span id="year"></span> KYCID. All rights reserved.
+      </p>
+      <p class="mb-4 ">
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+      </p>
+      <ul class="list-disc pl-6 mb-4 ">
+        <li>The above copyright notice and this permission notice shall be included in all
+          copies or substantial portions of the Software.</li>
+      </ul>
+      <p class="mb-4 ">
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+      </p>
+      <script>
+        document.getElementById('year').innerText = new Date().getFullYear();
+      </script>
+    </div>
+  </div>
+  
